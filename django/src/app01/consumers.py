0d1339e5503a2ec8341e04ed076afd9736fa31c2
Kiotto-code <yikwai1998@gmail.com>
2024-06-28 18:11:26 +0800

    able to prefix username fornt of message

--- django/src/app01/consumers.py
+++ django/src/app01/consumers.py
@@ -1,5 +1,11 @@
 from channels.generic.websocket import WebsocketConsumer
 from channels.exceptions import StopConsumer
+from django.utils.html import escape
+from django.urls import reverse
+
+
+CONST_LIST = []
+
 
 # ChatConsumer inherits from WebsocketConsumer. Also known as Polymorphism.
 # It is called by the routing.py file (routing.websocket_urlpatterns)
@@ -14,9 +20,27 @@ class ChatConsumer(WebsocketConsumer):
         # self.send("来了啊，老弟")
         
     def websocket_receive(self, message):
-        print(message)
-        self.send("收到了，老弟")
-        #self.close()  #-> 自动断开This is if server side want to disconnect the client (挥手）)
+        text = message['text']
+        # print("姐收到信息", text)
+        # print("Attributes of self:", dir(self))
+        # print("\n\n")
+        # print("Instance attributes of self:", vars(self))
+        # print("姐收到信息", message)
+        
+        
+        if " shabi " in text or "傻逼" in text or "关闭" in text:
+               self.send("服务器：你才是傻逼")
+               self.close()
+               return
+           
+        # user_id = self.scope['user'].id
+        # profile_url = reverse('user_profile', kwargs={'user_id': user_id})
+        # res = f"{self.scope['user'].profile.nickname}: {text}"
+        # name = self.scope['user'].username if self.scope['user'].is_authenticated else "Anonymous"
+
+        res = f"{text}SB"
+        
+        self.send(res)
 
     def websocket_disconnect(self, message):
         print("websocket_disconnect 被断链接了") # This is printed on the server side when the client disconnects
--- django/src/static/js/app01/chat.js
+++ django/src/static/js/app01/chat.js
@@ -7,7 +7,18 @@ function handleMessage(event){
     }
 }
 
-socket = new WebSocket("ws://127.0.0.1:8000/room/123/");
+// Determine the current host and protocol
+const host = window.location.hostname;
+const protocol = window.location.protocol === 'https:' ? 'wss:' : 'ws:';
+
+// Construct the WebSocket URL dynamically
+const port = window.location.port || (protocol === 'wss:' ? '443' : '80');
+const socketURL = `${protocol}//${host}:${port}/room/123/`;
+
+// Create a WebSocket connection
+const socket = new WebSocket(socketURL);
+// socket = new WebSocket("ws://127.0.0.1:8000/room/123/");
+
 
 socket.onopen = function(e){
     console.log("连接成功");
@@ -19,6 +30,7 @@ socket.onopen = function(e){
 
 //回掉函数 //当服务器发送消息过来的时候，会触发这个函数
 socket.onmessage = function(e){
+    scrollToBottom();
     let message = document.createElement("div");
     message.innerText = e.data;
     document.querySelector(".message").appendChild(message);
@@ -35,8 +47,35 @@ socket.onclose = function(e){
 }
 
 function sendMessage(){
-    let message = document.getElementById("txt");		
-    socket.send(message.value);
+    let message = document.getElementById("txt");
 
+    socket.send(nickname +  " :" + message.value);
+    // let messageToSend = `${request.user.id}: ${message.value}`;
+    // socket.send(messageToSend);
 
 }
+
+// function sendMessage() {
+//     let messageInput = document.getElementById("txt");
+//     let message = messageInput.value.trim();
+
+//     if (message) {
+//         // Construct the message to send
+//         let messageToSend = `${request.user.id}: ${message}`;
+        
+//         // Send the constructed message to the server
+//         socket.send(messageToSend.value);
+
+//         // Clear the input field after sending
+//         messageInput.value = '';
+//     }
+//     else
+//         socket.send(messageInput.value);
+
+// }
+
+function scrollToBottom() {
+    var messageContainer = document.getElementById('message-container');
+    messageContainer.scrollTop = messageContainer.scrollHeight;
+}
+

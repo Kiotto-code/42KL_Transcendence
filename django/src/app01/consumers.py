e3e5e787f13b0e07401958de35a6fd5d0a66cb5d
Kiotto-code <86033215+Kiotto-code@users.noreply.github.com>
2024-07-21 13:50:16 +0800

    Async WebSocketConsumer

--- django/src/app01/consumers.py
+++ django/src/app01/consumers.py
@@ -1,11 +1,9 @@
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.exceptions import StopConsumer
-from asgiref.sync import async_to_sync
+from asgiref.sync import sync_to_async
 from urllib.parse import parse_qs
 import re
 import json
-
-# django/src/static/images/meme/9299765.jpg
 
 TABLE = {
     'dog': '/static/images/meme/9299765.jpg',
@@ -14,39 +12,22 @@ TABLE = {
     'minion': 'https://miro.medium.com/v2/resize:fit:1000/format:webp/1*AmI9wRbXrfIWGESx6eEiTw.gif',
 }
 
-
 def is_online_image_url(url):
-    # Regular expression to match common image file extensions
     image_extensions = ['jpg', 'jpeg', 'png', 'gif', 'bmp', 'svg', 'webp']
-    pattern1 = r'^https?://.*\.(' + '|'.join(image_extensions) + r')'
-    pattern2 = r'^(?:data:image/(?:png|jpeg|gif|webp);base64,)'
-    pattern3 = r'^https?://images.*\.(' + '|'+ r')'
+    pattern = r'^https?://.*\.(' + '|'.join(image_extensions) + r')$'
+    return bool(re.match(pattern, url, re.IGNORECASE))
 
-    combined_pattern = f"({pattern1})|({pattern3})|({pattern2}.*)"
-    
-    # Compile the regex pattern
-    regex = re.compile(combined_pattern, re.IGNORECASE)
-    
-    # Check if the URL matches the pattern
-    if regex.match(url):
-        return True
-    else:
-        return False
-    
+class ChatConsumer(AsyncWebsocketConsumer):
 
-class ChatConsumer(WebsocketConsumer):
-    def check_if_static_image(self, text_data):
-        pattern = r'^:'  # Regex pattern to match ':=' exactly
-        
-        if re.match(pattern, text_data['message']):
-            response = TABLE.get(text_data['message'][1:], None)
-            if response:
-                self.send_image_message(response)
-            return True
+    async def websocket_connect(self, message):
+        self.chat_room = '123'
+        await self.accept()
+        if self.channel_layer is not None:
+            await self.channel_layer.group_add(self.chat_room, self.channel_name)
         else:
-            return False
-        
-    def websocket_receive(self, message):
+            print("Channel Layer return None")
+
+    async def websocket_receive(self, message):
         try:
             query_params = parse_qs(self.scope['query_string'].decode())
             self.customer_name = query_params.get('customer_name', ['Anonymous'])[0]
@@ -56,61 +37,31 @@ class ChatConsumer(WebsocketConsumer):
             if text_data['type'] == 'message':
                 if " shabi " in text_data['message'] or "傻逼" in text_data['message']:
                     text_data['message'] = f"服务器:【{self.customer_name}】你才是傻逼 "
-                    self.send(text_data['message'])
+                    await self.send(text_data['message'])
                 elif self.check_if_static_image(text_data):
                     return
                 elif is_online_image_url(text_data['message']):
                     response = text_data['message']
-                    self.send_image_message(response)
+                    await self.send_image_message(response)
                 else:
                     text_data['message'] = text_data['name'] + ": " + text_data['message']
-                    self.send_chat_message(text_data['message'])
+                    await self.send_chat_message(text_data['message'])
             elif text_data['type'] == 'image':
-                self.send_image_message(text_data['image'])
+                await self.send_image_message(text_data['image'])
 
         except json.JSONDecodeError as e:
             error_message = {'type': 'error', 'message': 'Invalid JSON format in received message.'}
-            self.send(json.dumps(error_message))
+            await self.send(json.dumps(error_message))
         except Exception as e:
             error_message = {'type': 'error', 'message': str(e)}
-            self.send(json.dumps(error_message))
+            await self.send(json.dumps(error_message))
 
-    def websocket_connect(self, message):
-        self.chat_room = '123'
-        self.accept()
-        if self.channel_layer is not None:
-            async_to_sync(self.channel_layer.group_add)(self.chat_room, self.channel_name)
-        else:
-            print("Channel Layer return None")
-        
-
-    # def websocket_receive(self, message):
-    #     query_params = parse_qs(self.scope['query_string'].decode())
-    #     self.customer_name = query_params.get('customer_name', ['Anonymous'])[0]
-
-    #     text_data = json.loads(message['text'])
-
-    #     if text_data['type'] == 'message':
-    #         if " shabi " in text_data['message'] or "傻逼" in text_data['message']:
-    #             text_data['message'] = f"服务器:【{self.customer_name}】你才是傻逼 "
-    #             self.send(text_data['message'])
-    #         elif self.check_if_static_image(text_data):
-    #             return
-    #         elif is_online_image_url(text_data['message']):
-    #             response = text_data['message']
-    #             self.send_image_message(response)
-    #         else:
-    #             text_data['message'] = text_data['name'] + ": " + text_data['message']
-    #             self.send_chat_message(text_data['message'])
-    #     elif text_data['type'] == 'image':
-    #         self.send_image_message(text_data['image'])
-
-    def websocket_disconnect(self, message):
-        async_to_sync(self.channel_layer.group_discard)(self.chat_room, self.channel_name)
+    async def websocket_disconnect(self, message):
+        await self.channel_layer.group_discard(self.chat_room, self.channel_name)
         raise StopConsumer()
 
-    def send_chat_message(self, message):
-        async_to_sync(self.channel_layer.group_send)(
+    async def send_chat_message(self, message):
+        await self.channel_layer.group_send(
             self.chat_room,
             {
                 "type": "chat.message",
@@ -118,11 +69,10 @@ class ChatConsumer(WebsocketConsumer):
             }
         )
 
-    def send_image_message(self, image_data):
-        # self.customer_name = self.scope['query_string'].decode().split('=')[1]
+    async def send_image_message(self, image_data):
         query_params = parse_qs(self.scope['query_string'].decode())
         self.customer_name = query_params.get('customer_name', ['Anonymous'])[0]
-        async_to_sync(self.channel_layer.group_send)(
+        await self.channel_layer.group_send(
             self.chat_room,
             {
                 "type": "image.message",
@@ -131,15 +81,25 @@ class ChatConsumer(WebsocketConsumer):
             }
         )
 
-    def chat_message(self, event):
-        self.send(text_data=json.dumps({
+    async def chat_message(self, event):
+        await self.send(text_data=json.dumps({
             'type': 'message',
             'message': event['message']
         }))
 
-    def image_message(self, event):
-        self.send(text_data=json.dumps({
+    async def image_message(self, event):
+        await self.send(text_data=json.dumps({
             'type': 'image',
             'name': event['name'],
             'image': event['image']
         }))
+
+    def check_if_static_image(self, text_data):
+        pattern = r'^:'  # Regex pattern to match ':=' exactly
+        if re.match(pattern, text_data['message']):
+            response = TABLE.get(text_data['message'][1:], None)
+            if response:
+                asyncio.create_task(self.send_image_message(response))
+            return True
+        else:
+            return False
